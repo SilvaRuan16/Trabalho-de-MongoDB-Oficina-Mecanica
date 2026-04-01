651ceab796bfcf52d5b7ade5aa9c681ca9f7c2b8
davs22 <mdiirr2003@gmail.com>
2026-04-01 02:58:27 -0300

    Feat: Criacao das ultimas consultas e adicao de print

--- produto1_oficina_mecanica.mongodb.js
+++ produto1_oficina_mecanica.mongodb.js
@@ -24,7 +24,11 @@ db.createCollection('servicos');
 db.veiculos.deleteMany({});
 db.servicos.deleteMany({});
 
-// Insere os dados sobre o veículo
+// --------------------------------------------------
+// 2. INSERÇÃO DE DADOS
+// --------------------------------------------------
+
+// 2.1 Insere os dados sobre o veículo
 db.veiculos.insertMany([
     {
         _id: 1,
@@ -178,7 +182,7 @@ db.veiculos.insertMany([
     }
 ]);
 
-// Insere os dados sobre os serviços realizados
+// 2.2 Insere os dados sobre os serviços realizados
 db.servicos.insertMany([
     {
         tipo: 'Troca de oleo',
@@ -318,76 +322,150 @@ db.servicos.insertMany([
     }
 ]);
 
-// Este comando é responsavel por buscar todos os veiculos que possuem um ano de fabricação maior ou igual a 2016.
-db.veiculos.find({ ano: { $gte: 2016 } }).pretty()
+// --------------------------------------------------
+// 3. CONSULTAS COM OPERADORES DE COMPARAÇÃO
+// --------------------------------------------------
 
-// Este comando é responsavel por buscar todos os servicos cujo valor seja menor que 100 reais.
-db.servicos.find({ valor: { $lt: 100.00 } }).pretty()
+print("==================================================")
+print("BANCO DE DADOS: oficinaMecanica")
+print("COLEÇÕES: veiculos, servicos")
+print("==================================================")
 
-// Este comando é responsavel por buscar todos os veiculos que possuem um id superior a 4.
-db.veiculos.find({ _id: { $gt: 4 } }).pretty()
+// 3.1 $gte - (Este comando é responsavel por buscar todos os veiculos que possuem um ano de fabricação maior ou igual a 2016)
+print("--- 3.1 $gte - (Este comando é responsavel por buscar todos os veiculos que possuem um ano de fabricação maior ou igual a 2016) ---");
+printjson(db.veiculos.find({ ano: { $gte: 2016 } }).toArray());
 
-// Este comando é responsavel por buscar todos os veiculos cujo quilometragem é menor ou igual a 100mil.
-db.veiculos.find({ quilometragem: { $lte: 100_000 } }).pretty()
+// 3.2 $lt - (Este comando é responsavel por buscar todos os servicos cujo valor seja menor que 100 reais)
+print("--- 3.2 $lt - (Este comando é responsavel por buscar todos os servicos cujo valor seja menor que 100 reais) ---");
+printjson(db.servicos.find({ valor: { $lt: 100.00 } }).toArray());
 
-// Este comando lista os servicos cujo o id foi selecionado e depois busca quais são os respectivos veiculos daquele serviço.
-db.servicos.aggregate([
+// 3.3 $gt - (Este comando é responsavel por buscar todos os veiculos que possuem um id superior a 4)
+print("--- 3.3 $gt - (Este comando é responsavel por buscar todos os veiculos que possuem um id superior a 4) ---");
+printjson(db.veiculos.find({ _id: { $gt: 4 } }).toArray());
+
+// 3.4 $lte - (Este comando é responsavel por buscar todos os veiculos cujo quilometragem é menor ou igual a 100mil)
+print("--- 3.4 $lte - (Este comando é responsavel por buscar todos os veiculos cujo quilometragem é menor ou igual a 100mil) ---");
+printjson(db.veiculos.find({ quilometragem: { $lte: 100_000 } }).toArray());
+
+// 3.5 $in - (Este comando lista os servicos cujo o id foi selecionado e depois busca quais são os respectivos veiculos daquele serviço)
+print("--- 3.5 $in - (Este comando é responsavel por listar os servicos cujo o id foi selecionado e depois busca quais são os respectivos veiculos daquele serviço) ---");
+printjson(db.servicos.aggregate([
     {
         $match: {
-            veiculoId: {$in: [5, 3, 8]}
-        }
-    }, 
-    {
-        $lookup: {
-          from: "veiculos",
-          localField: "veiculoId",
-          foreignField: "_id",
-          as: "veiculos"
+            veiculoId: { $in: [5, 3, 8] }
         }
     },
-]).pretty()
+    {
+        $lookup: {
+            from: "veiculos",
+            localField: "veiculoId",
+            foreignField: "_id",
+            as: "veiculos"
+        }
+    },
+]).toArray());
 
-// Este comando filtra somente os servicos que ainda não foram finalizados e mostra o veiculo referente.
-db.servicos.aggregate([
+// 3.6 $ne - (Este comando filtra somente os servicos que ainda não foram finalizados e mostra o veiculo referente)
+print("--- 3.6 $ne - (Este comando é responsavel por filtrar somente os servicos que ainda não foram finalizados e mostrar o veiculo referente) ---");
+printjson(db.servicos.aggregate([
     {
         $match: {
-            concluido: {$ne: true}
+            concluido: { $ne: true }
         },
     },
     {
         $lookup: {
-          from: "veiculos",
-          localField: "veiculoId",
-          foreignField: "_id",
-          as: "veiculos"
+            from: "veiculos",
+            localField: "veiculoId",
+            foreignField: "_id",
+            as: "veiculos"
         }
     },
-]).pretty()
+]).toArray());
 
 // --------------------------------------------------
 // 4. CONSULTAS COM OPERADORES LÓGICOS
 // --------------------------------------------------
 
-//4.1 $and (implicito) - (Busca por serviços de troca de pastilhas que foram concluídos)
-db.servicos.find({ concluido: true, valor: { $gte: 200 } }).pretty();
+// 4.1 $and (implicito) - (Busca por serviços concluídos com valor maior ou igual a 200.00)
+print("--- 4.1 $and (implicito) - (Busca por serviços concluídos com valor maior ou igual a 200.00) ---");
+printjson(db.servicos.find({ concluido: true, valor: { $gte: 200.00 } }).toArray());
 
-//4.2 $and (explicito) - (Busca por serviços aonde foi concluido ou não e que sejam do tipo troca de velas ou revisão geral)
-db.servicos.find({
+// 4.2 $and (explicito) - (Busca por serviços aonde foi concluido ou não e que sejam do tipo troca de velas ou revisão geral)
+print("--- 4.2 $and (explicito) - (Busca por serviços aonde foi concluido ou não e que sejam do tipo troca de velas ou revisão geral) ---");
+printjson(db.servicos.find({
     $and: [{ $or: [{ concluido: true }, { concluido: false }] },
     { $or: [{ tipo: 'Troca de velas' }, { tipo: 'Revisao geral' }] }]
-}).pretty();
+}).toArray());
 
-//4.3 $or - (Busca por serviços do tipo troca de oleo ou revisão de freios)
-db.servicos.find({
+// 4.3 $or - (Busca por serviços do tipo troca de oleo ou revisão de freios)
+print("--- 4.3 $or - (Busca por serviços do tipo troca de oleo ou revisão de freios) ---");
+printjson(db.servicos.find({
     $or: [{ tipo: 'Troca de oleo' }, { tipo: 'Revisao de freios' }]
-}).pretty();
+}).toArray());
 
-//4.4 $nor - (Busca por veículos que não sejam da cor preta ou branca)
-db.veiculos.find({
+// 4.4 $nor - (Busca por veículos que não sejam da cor preta ou branca)
+print("--- 4.4 $nor - (Busca por veículos que não sejam da cor preta ou branca) ---");
+printjson(db.veiculos.find({
     $nor: [{ cor: 'Preto' }, { cor: 'Branco' }]
-}).pretty();
+}).toArray());
 
-//4.5 $not - (Busca por veículos que não sejam do ano de fabricação 2010 ou inferior)
-db.veiculos.find({
-   ano: { $not: { $lte: 2010 } }
-}).pretty();
+// 4.5 $not - (Busca por veículos que não sejam do ano de fabricação 2010 ou inferior)
+print("--- 4.5 $not - (Busca por veículos que não sejam do ano de fabricação 2010 ou inferior) ---");
+printjson(db.veiculos.find({
+    ano: { $not: { $lte: 2010 } }
+}).toArray());
+
+// 4.6 $and + $or - (Busca por veículos da marca Volkswagen que sejam da cor preta ou branca)
+print("--- 4.6 $and + $or - (Busca por veículos da marca Volkswagen que sejam da cor prata ou azul) ---");
+printjson(db.veiculos.find({
+    $and: [{ marca: "Volkswagen" }, {}]
+}).toArray());
+
+// 4.7 Combinacoes - (Busca por serviços não concluídos com valor maior ou igual a 300.00 e data de serviço dentro de um data antiga ou mais atual)
+print("--- 4.7 Combinacoes - (Busca por serviços não concluídos com valor maior ou igual a 300.00 e data de serviço dentro de um data antiga ou mais atual) ---");
+printjson(db.servicos.find({
+    concluido: false, valor: { $gte: 300.00 },
+    $or: [
+        { dataServico: { $lte: new Date("2026-04-30") } },
+        { dataServico: { $gte: new Date("2026-05-05") } }
+    ]
+}).toArray());
+
+// 4.8 $and - (Busca por serviços do tipo troca de oleo que foram concluídos e mostra o veículo relacionado)
+print("--- 4.8 $and - (Busca por serviços do tipo troca de oleo que foram concluídos e mostra o veículo relacionado) ---");
+printjson(db.servicos.aggregate([
+    {
+        $match: {
+            $and: [{
+                tipo: 'Troca de oleo', concluido: true
+            }]
+        }
+    },
+    {
+        $lookup: {
+            from: "veiculos",
+            localField: "veiculoId",
+            foreignField: "_id",
+            as: "veiculos"
+        }
+    },
+]).toArray());
+
+// 4.9 $or - (Busca por veículos da cor azul ou preta e mostra os serviços relacionados a esses veículos)
+print("--- 4.9 $or - (Busca por veículos da cor azul ou preto e mostra os serviços relacionados a esses veículos) ---");
+printjson(db.veiculos.aggregate([
+    {
+        $match: {
+            $or: [{ cor: 'Preto' }, { cor: 'Azul' }]
+        }
+    },
+    {
+        $lookup: {
+            from: "servicos",
+            localField: "_id",
+            foreignField: "veiculoId",
+            as: "historico_servicos"
+        }
+    },
+]).toArray());
